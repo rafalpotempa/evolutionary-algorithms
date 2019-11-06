f1a3e618f65af9947ed7e4d799ad86f11eeac55c
Rafał Potempa <rafal.potem@gmail.com>
2019-11-06 14:12:06 +0100

    Add time benchmark

--- algorithm.py
+++ algorithm.py
@@ -1,14 +1,16 @@
 from helpers import *
 from random import choices
+from time import time
 
 # load data
 data = loadData()
-mu = len(data)
+mu = 101
 
 # initial population N = 101
 population = initPopulation(mu)
 previousError = 1e10
 
+start = time()
 for generation in range(150):
 	parents = choices(population, k=mu*5) # parents population N = 505
 	offsprings = mutate(parents) 
@@ -17,9 +19,11 @@ for generation in range(150):
 	error = min([pop['error'] for pop in population])
 	print(f"{generation:3}: {error:.3f}")
 	
-	if abs(error - previousError) < 1e-3:
+	if abs(error - previousError) < 1e-5:
 		break
 	previousError = error
+end = time()
 
 print(population[0])
 plotResults(data, population[0])
+print(f"Time elapsed: {end - start} s")
